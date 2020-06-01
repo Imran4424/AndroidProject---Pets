7e046031a719a957cdf91512037a23db9f0d90f0
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-01 16:20:38 +0600

    pet db helper

--- app/src/main/java/com/luminous/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/luminous/android/pets/data/PetDbHelper.java
@@ -1,12 +1,27 @@
 package com.luminous.android.pets.data;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
 
 /**
  * Created by Shah Md Imran Hossain on 01, June, 2020
  */
 public class PetDbHelper extends SQLiteOpenHelper {
+
+    /** Name of the database file */
+    private final static String DATABASE_NAME = "shelter.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private final static int DATABASE_VERSION = 1;
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
