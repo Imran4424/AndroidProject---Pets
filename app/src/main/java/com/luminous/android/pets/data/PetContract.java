b509a3df495e87e26b577d4795d1c6634f3ae137
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-01 13:46:39 +0600

    pet contract

--- app/src/main/java/com/luminous/android/pets/data/PetContract.java
+++ app/src/main/java/com/luminous/android/pets/data/PetContract.java
@@ -1,7 +1,13 @@
 package com.luminous.android.pets.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by Shah Md Imran Hossain on 01, June, 2020
  */
-public class PetContract {
+public final class PetContract {
+
+    public static final class PetEntry implements BaseColumns {
+        
+    }
 }
