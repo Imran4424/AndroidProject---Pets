becbedeb36289df4ad849cff67aa084cb61efd02
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-01 16:14:39 +0600

    pet db helper

--- app/src/main/java/com/luminous/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/luminous/android/pets/data/PetDbHelper.java
@@ -1,8 +1,20 @@
 package com.luminous.android.pets.data;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Shah Md Imran Hossain on 01, June, 2020
  */
-public class PetDbHelper {
+public class PetDbHelper extends SQLiteOpenHelper {
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
